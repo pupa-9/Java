854ce746d7f2aa5ad4cd0d0231800e7ae2de6f10
Ярослав <108534787+pupa-9@users.noreply.github.com>
2023-08-09 09:35:23 +0400

    Lesson5

---
+++ Lesson5.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class WordCount {
+    public static void main(String[] args) {
+        String text = "Россия идет вперед всей планеты. Планета — это не Россия.";
+        text = text.toLowerCase(); // приводим к нижнему регистру
+        String[] words = text.split("[^а-яА-ЯёЁ]+"); // разбиваем текст на слова
+
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                Integer count = wordCount.get(word);
+                if (count == null) {
+                    count = 0;
+                }
+                wordCount.put(word, count + 1);
+            }
+        }
+
+        // выводим результаты
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
